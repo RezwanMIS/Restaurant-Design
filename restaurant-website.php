6d9f840cc26c20a8aa97e4be1d995e55077dd86e
Rezwan Sheikh <139754574+RezwanMIS@users.noreply.github.com>
2024-12-14 17:12:33 +0600

    Add html and css file fot the Restaurant

---
+++ restaurant-website.php
@@ -0,0 +1,296 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Restaurant</title>
+
+      <!-- CDN font awesome -->
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- CDN font awesome -->
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+   <link href="https://fonts.googleapis.com/css2?family=Handlee&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+
+   
+ <link rel="stylesheet" href="style.css.css">
+</head>
+<body>
+
+<div class="container">
+
+<!-- header start -->
+<header>
+       <div class="row">
+            <div class="col-lg-4">
+                 <div class="logo-div">
+                    <a href="#">
+                        <i class="logo fas fa-utensils fa-3x"></i>
+                    </a>
+                 </div>
+            </div>
+
+            <div class="col-lg-8">
+                 <nav>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#about-us">About Us</a></li>
+                        <li><a href="#menu-section">Food Menu</a></li>
+                        <li><a href="#contact">Contact</a></li>
+
+                    </ul>
+                 </nav>
+            </div>
+       </div>
+</header>
+<!-- header end -->
+
+<main>
+
+<!-- banner section start -->
+     <section id="banner-section" >
+     <div class="banner-contents">
+     <h1>Spicy King Restaurant</h1>
+          <p>20% discounts for take away</p>
+          <button class="btn" >Order Now</button>
+     </div>
+     </section>
+<!-- banner section end -->
+
+
+<!-- About section start -->
+
+<section id="about-us">
+     <div class="row">
+          <div class="col-lg-12">
+               <div class="about-us-content">
+                    <h3>About US</h3>
+                    <p>Welcome to Spicy King Restaurant, where passion for food and a commitment to exceptional service come together. Our journey began in [2024] with a simple goal: to create a dining experience that brings people together through delicious, handcrafted meals and a warm, inviting atmosphere.
+
+                     At Spicy King Restaurant, we pride ourselves on using the freshest local ingredients to create dishes that celebrate the rich flavors of helthy cuisine. Whether you're here for a casual meal with friends or a special evening with loved ones, our team works tirelessly to provide you with an unforgettable dining experience.
+
+                     Our chefs, inspired by both traditional and innovative techniques, craft every meal with care and creativity. From Finest Addict Dine to our seasonal specials, each dish is designed to offer something unique for every palate.
+
+                     We believe that dining is about more than just food—it's about creating memories, forging connections, and sharing moments of joy. Join us at Spicy King Restaurantand discover why we’re the perfect place for any occasio</p>
+               </div>
+          </div>
+     </div>
+
+</section>
+
+<!-- About section end -->
+
+
+<!-- food menu section start -->
+<section id="menu-section">
+     <div class="row">
+          <div class="col-lg-3 col-md-6 col-sm-12">
+               <div class="card">
+                    <div class="card-body">
+                         <img src="picture/food2.jpg.jpg" alt="">
+
+                       <div class="item-desc">
+                         <h3 class="item-name">Fruts Width Simplicity</h3>
+                         <p>Item NO: 101</p>
+                         <p>Rating: 4/5</p>
+                         <p>Price: 250 TK.</p>
+                       </div>
+                    </div>
+               </div>
+
+          </div>
+
+
+          <div class="col-lg-3 col-md-6 col-sm-12">
+               <div class="card">
+                    <div class="card-body">
+                         <img src="picture/food1.jpg.jpg" alt="">
+
+                       <div class="item-desc">
+                         <h3 class="item-name">Egge Everest</h3>
+                         <p>Item NO: 102</p>
+                         <p>Rating: 4/5</p>
+                         <p>Price: 230 TK.</p>
+                       </div>
+                    </div>
+               </div>
+
+          </div>
+
+          <div class="col-lg-3 col-md-6 col-sm-12">
+               <div class="card">
+                    <div class="card-body">
+                         <img src="picture/food3.jpg.jpg" alt="">
+
+                       <div class="item-desc">
+                         <h3 class="item-name">Vegans Vessel Food</h3>
+                         <p>Item NO: 103</p>
+                         <p>Rating: 4/5</p>
+                         <p>Price: 320 TK.</p>
+                       </div>
+                    </div>
+               </div>
+
+          </div>
+
+          <div class="col-lg-3 col-md-6 col-sm-12">
+               <div class="card">
+                    <div class="card-body">
+                         <img src="picture/food4.jpg" alt="">
+
+                       <div class="item-desc">
+                         <h3 class="item-name">Urban Delight</h3>
+                         <p>Item NO: 104</p>
+                         <p>Rating: 4/5</p>
+                         <p>Price: 240 TK.</p>
+                       </div>
+                    </div>
+               </div>
+
+          </div>
+     </div>
+
+
+     <section>
+
+     <div class="row">
+          <div class="col-lg-3 col-md-6 col-sm-12">
+               <div class="card">
+                    <div class="card-body">
+                         <img src="picture/food8.jpg" alt="">
+
+                       <div class="item-desc">
+                         <h3 class="item-name">Warm Water Upland</h3>
+                         <p>Item NO: 105</p>
+                         <p>Rating: 4/5</p>
+                         <p>Price: 150 TK.</p>
+                       </div>
+                    </div>
+               </div>
+
+          </div>
+
+
+          <div class="col-lg-3 col-md-6 col-sm-12">
+               <div class="card">
+                    <div class="card-body">
+                         <img src="picture/food6.jpg" alt="">
+
+                       <div class="item-desc">
+                         <h3 class="item-name">Finest Addict Dine</h3>
+                         <p>Item NO: 106</p>
+                         <p>Rating: 4/5</p>
+                         <p>Price: 350 TK.</p>
+                       </div>
+                    </div>
+               </div>
+
+          </div>
+
+          <div class="col-lg-3 col-md-6 col-sm-12">
+               <div class="card">
+                    <div class="card-body">
+                         <img src="picture/food7.jpg" alt="">
+
+                       <div class="item-desc">
+                         <h3 class="item-name">Oxone Oceanus</h3>
+                         <p>Item NO: 107</p>
+                         <p>Rating: 4/5</p>
+                         <p>Price: 260 TK.</p>
+                       </div>
+                    </div>
+               </div>
+
+          </div>
+
+          <div class="col-lg-3 col-md-6 col-sm-12">
+               <div class="card">
+                    <div class="card-body">
+                         <img src="picture/food5.jpg" alt="">
+
+                       <div class="item-desc">
+                         <h3 class="item-name">Breakfast Champs</h3>
+                         <p>Item NO: 108</p>
+                         <p>Rating: 4/5</p>
+                         <p>Price: 380 TK.</p>
+                       </div>
+                    </div>
+               </div>
+
+          </div>
+     </div>
+
+
+     </section>
+
+</section>
+<!-- food menu section end -->
+
+<!-- Contact Section start -->
+<section id="contact">
+     <div class="row">
+          <div class="col-lg-8">
+          <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7293.999435741465!2d90.39080319503229!3d23.92506407121076!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c493175943d7%3A0x52fac0bf2fa9947b!2z4KaX4Ka-4Kac4KeA4Kaq4KeB4Kaw4Ka-LCDgpp_gppngp43gppfgp4A!5e0!3m2!1sbn!2sbd!4v1732960073366!5m2!1sbn!2sbd" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          </div>
+
+          <div class="col-lg-4">
+               <div class="contact-details">
+                    <h3>Contact Details</h3>
+                    <br>
+                    <div>
+                         <h5>Spicy King Restaurant</h5>
+                         <address>
+                              Sumon Market, Kazibari road,
+                              Gazipura, Tongi, Gazipur.
+                         </address>
+                    </div>
+                    <div>
+                         <br>
+                         <h5>Phone: </h5>
+                         <p>+01960634996</p>
+                    </div>
+                    <br>
+                    <div>
+                         <h5>Opening Hours</h5>
+                         <p>Tue-sun: 5pm to 11pm
+                         Inc. Bank Holidays
+                         </p>
+                    </div>
+               </div>
+
+          </div>
+     </div>
+
+</section>
+
+<!-- Contact Section start -->
+
+</main>
+
+<footer>
+     <div class="row">
+          <div class="col-lg-6">
+               <p>&copy; All rights reserved by Spicy King Restaurant</p>
+          </div>
+
+          <div class="col-lg-6">
+
+          <div class="social-menu">
+               <a href="#"><i class="icon-style fas fa-arrow-up" ></i></a>
+               <a href="#"><i class="icon-style fab fa-facebook-f" ></i></a>
+               <a href="#"><i class="icon-style fab fa-youtube" ></i></a>
+               <a href="#"><i class="icon-style fab fa-twitter" ></i></a>
+               <a href="#"><i class="icon-style fab fa-instagram" ></i></a>
+
+          </div>
+
+          </div>
+     </div>
+
+</footer>
+</div>
+    
+</body>
+</html>
